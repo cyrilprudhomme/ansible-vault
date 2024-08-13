46b5d4c2d67c79e26da8be3e8329ebf5dd93a56d
cprudhomme <cyril.prudhomme.ext@cibtp.fr>
2024-08-13 10:28:19 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ansible-vault
+# Ansible Vault Tool
+This is a simple online **Ansible Vault** encryption/decryption tool. 
+
+You may encrypt or decrypt any information using online interface or api tools provided.
+
+https://ansible-vault.braz.dev/
+
+# how to use
+1. This is nodejs+express app, so use: npm install && npm run
+2. You may also use docker setup:
+      1. cd docker
+      2. docker-compose up -d
+      3. localhost:3003
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/' , function(req, res, next) {
+  let the_host = req.headers['x-forwarded-host'] || req.headers.host
+  let config_gtag = process.env.CONFIG_GTAG
+  let config_adsense = process.env.CONFIG_ADSENSE
+  let config_repo = process.env.CONFIG_REPO || 'https://github.com/fbraz3/ansible-vault-tool'
+
+  res.render('index', {
+    host: the_host,
+    config_gtag: config_gtag,
+    config_adsense: config_adsense,
+    config_repo: config_repo
+  });
+});
+
+module.exports = router;
